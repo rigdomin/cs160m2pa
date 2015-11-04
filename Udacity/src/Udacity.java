3c326a9e4096f7ca6101430de95145837ffc378f
Rigo Dominguez <rigdomin@users.noreply.github.com>
2015-11-03 18:58:49 -0800

    Updated as of 11/3/2015 @ 7:00pm
    LEFT TO SCRAPE:
    video link
    start date
    category
    site
    language
    certificate
    university
    time scraped
    
    RETRIEVED:
    title
    short description
    full description
    course link
    course image url
    
    CANNOT SCRAPE DUE TO SCRIPT GENERATED CONTENT:
    String course_length;
    int course_fee;

--- Udacity/src/Udacity.java
+++ Udacity/src/Udacity.java
@@ -27,6 +27,7 @@ public class Udacity
 		ArrayList<String> links = new ArrayList<String>();
 		//int i = 0;
 		
+		//READS ALL URLs FROM TEXTFILE
 		try{
 			FileReader fi = new FileReader("src/UdacityCourselinks.txt");
 			BufferedReader bf = new BufferedReader(fi);
@@ -45,31 +46,48 @@ public class Udacity
 			System.out.println("File Not Found");
 		}
 		
+		//JSOUP CODE TO RETRIEVE DATA
 		String url = null;
 		Document doc;
 		Elements currElem;
 		String title, startDate, shortDesc, fullDesc, price, crsImg, instructor;
+		String duration = null, durationType;
 		ArrayList<String> instructors = new ArrayList<String>();
 		String []tmpAry;
 		int i = 0;
 		
 		for(; i < links.size(); i++)
 		{
+			//clears instructors list for next page
+			instructors.clear();
+			
+			//CONTAINS COURSE URL
 			url = links.get(i);
 			//System.out.println(url);
+			
 			doc = Jsoup.connect(url).timeout(0).get(); 
 			
-
+			//RETREIVES COURSE TITLE
 			title = doc.select("h1[class=course-header-title h-slim]").text();
+			
+			//RETREIVES COURSE FULL DESCRIPTION
 			fullDesc = doc.select("div[class=pretty-format]").select("p").text();
+			
+			//RETREIVES COURSE SHORT DESCRIPTION
 			shortDesc = doc.select("meta[name=description]").first().attr("content");
 			
+			//RETREIVES COURSE IMAGE URL
 			crsImg = doc.select("div.container.banner-content").attr("style");
-			if(!crsImg.matches(".*url\\(\\)"))
+			//System.out.println(">>>>" + crsImg);
+			if(crsImg.contains("url") && !crsImg.matches(".*url\\(\\)"))
 				crsImg = crsImg.split("url\\(")[1].split("\\)")[0];
 			else
+			{
+				//System.out.println("CHECK " + doc.select("img").text());
 				crsImg = "N/A";
+			}
 			
+			//RETREIVES COURSE INSTRUCTORS
 			currElem = doc.select("div.col-xs-12.instructor-information.pull-left");
 			//split("\\9662 \\9662 ");
 			tmpAry = currElem.select("a").text().split("([^\\u9662] ){2,}");
@@ -82,15 +100,24 @@ public class Udacity
 				}
 			}
 			
+			//RETREIVES COURSE DURATION
+			if(doc.text().contains("expectedDuration: "))
+				duration = doc.text().split("expectedDuration: ")[1];
+			if(duration != null)
+				System.out.println("DUR: " + duration);
+			
+			
+			/*****************************************************************************************/
 			//price = doc.select("div[class=media-body]").select("strong").text();  //SCRIPT GENERATED
 			//startDate = doc.select("strong[class=ng-binding]").text();			//SCRIPT GENERATED
 			
 			
 			System.out.println(title);
-			System.out.println("\tDESCR:       " + fullDesc.substring(0, 19));
-			System.out.println("\tSMALLDESC:   " + shortDesc.substring(0,19));
-			System.out.println("\tIMAGE URL:   " + crsImg);
-			System.out.println("\tINSTRUCTORS: " + instructors);
+			//System.out.println("\tDESCR:       " + fullDesc);
+			//System.out.println("\tSMALLDESC:   " + shortDesc);
+			//System.out.println("\tIMAGE URL:   " + crsImg);
+			//System.out.println("\tINSTRUCTORS: " + instructors);
+			
 			//System.out.println("\tPRICE: " + price);								//SCRIPT GENERATED
 			//System.out.println("\t" + startDate);									//SCRIPT GENERATED
 			
