7ad389277c9156d556f35b539ce865324f450f21
Rigo Dominguez <rigdomin@users.noreply.github.com>
2015-11-08 20:39:46 -0800

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,28 @@
+
+<?php include('header.php'); ?>
+
+		<div id="container">
+			<div id="category_box">
+				<a href=""><img src="img/courses_logo.png" alt="courses_logo"/></a>
+				<h3>COURSES</h3>
+				<p>These courses are for...</p>
+			</div>
+			<div id="category_box">
+				<a href=""><img src="img/profession_logo.png" alt="profession_logo"/></a>
+				<h3>PROFESSIONS</h3>
+				<p>These professions are in demand right now.</p>
+			</div>
+			<div id="category_box">
+				<a href=""><img src="img/moocs_logo.png" alt="moocs_logo"/></a>
+				<h3>MOOCS</h3>
+				<p>Our courses listings were retreived from these locatins.</p>
+			</div>
+		</div>
+		<div id="container">
+			<h2>Our Goal</h2>
+		</div>
+		<div id="container">
+			<h2>Our Services</h2>
+		</div>
+		
+<?php include('footer.php'); ?>
