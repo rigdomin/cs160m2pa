3c1e5abfce92f425d97087071b39d257e83ac972
Rigo Dominguez <rigdomin@users.noreply.github.com>
2015-11-08 20:40:39 -0800

    Create footer.php

---
+++ web/footer.php
@@ -0,0 +1,38 @@
+
+			<footer>
+				<div id="footerContent">
+					<div id="footerList">
+						<p>Resources</p>
+						<hr/>
+						<ul>
+							<li><a href="">Services</a></li>
+							<li><a href="">Partners</a></li>
+							<li><a href="">Others</a></li>
+						</ul>
+					</div>
+					<div id="footerList">
+						<p>Career Opportunities</p>
+						<hr/>
+						<ul>
+							<li><a href="">Internship</a></li>
+							<li><a href="">Web Developer</a></li>
+							<li><a href="">Moderator</a></li>
+							<li><a href="">Customer Service Rep</a></li>
+						</ul>
+					</div>
+					<div id="footerList">
+						<p>Support</p>
+						<hr/>
+						<ul>
+							<li><a href="">Customer Service</a></li>
+							<li><a href="">FAQs</a></li>
+						</ul>
+					</div>
+				</div>
+				<p id="copyright">
+					Copyright CS160_Group2 Web Designs. All Rights Reserved.
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
